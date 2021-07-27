05923f1ead036ad33bd5c3d0e23752e90a7e4fd6
Lukas Nguyen <lukas.nguyen@contemi.com>
2021-07-27 17:48:19 +0700

    - Added backet project. - Implemented business login within BasketRepository.

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -7,9 +7,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Catalog.API
+namespace Basket.API
 {
-    public static class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
